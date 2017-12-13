66e6810de6575cd334069e0867bccd7b8b261b8c
zhaopeng <mrzhao90@126.com>
2017-12-13 16:46:39 +0800

    提交模拟数据

---
+++ ZPKits/ZPKits/Class/ZPObject.h
@@ -0,0 +1,13 @@
+//
+//  ZPObject.h
+//  ZPKits
+//
+//  Created by 赵鹏 on 2017/12/13.
+//  Copyright © 2017年 赵鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZPObject : NSObject
+
+@end
---
+++ ZPKits/ZPKits/Class/ZPObject.m
@@ -0,0 +1,13 @@
+//
+//  ZPObject.m
+//  ZPKits
+//
+//  Created by 赵鹏 on 2017/12/13.
+//  Copyright © 2017年 赵鹏. All rights reserved.
+//
+
+#import "ZPObject.h"
+
+@implementation ZPObject
+
+@end
